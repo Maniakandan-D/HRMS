b5675f17c37655c77f8d640b1ee12edbf02a3988
Manikandandhanendhiran <manikandan.dhanen@gmail.com>
2022-10-20 10:05:09 +0530

    user profile

--- src/app/user/shared/user-profile.service.ts
+++ src/app/user/shared/user-profile.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
-import { UserBasicInfo, UserContactInfo, UserProfile } from './user.model';
+import { UserBasicInfo, UserContactInfo, UserProfile  } from './user-profile.model';
 
 @Injectable({
   providedIn: 'root'
