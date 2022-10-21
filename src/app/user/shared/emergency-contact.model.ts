3ddb47a324c93dec26b7b6280bfd337d55bd655f
Manikandandhanendhiran <manikandan.dhanen@gmail.com>
2022-10-21 15:35:24 +0530

    bank form added

---
+++ src/app/user/shared/emergency-contact.model.ts
@@ -0,0 +1,16 @@
+export class EmergencyContactInfo {
+    id: string;
+    Physician: string;
+    phoneNumber: number;
+    bloodGroup: string;
+    firstName: string;
+    lastName: string;
+    relationship: string;
+    street: string;
+    city: string;
+    state: string;
+    pincode: string;
+    homePhone: number;
+    workPhone: number;
+    mobilePhone: number;
+}
